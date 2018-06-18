e9d56e71ca16cddf8e2be650247b4baff9ad2724
stephendeanharper <stephendeanharper@gmail.com>
2018-06-17 20:28:00 -0700

    Updated readme, coded what i could. couldnt figure out how to get the json file to do what it needs to

---
+++ Lab08_LINQinManhatten/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab08_LINQinManhatten
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoadJSON();
+
+        }
+
+        public static void LoadJSON()
+        {
+            string path = "data.json";
+
+            //Properties properties = JsonConvert.DeserializeObject<Properties>(path);
+
+            using (StreamReader r = new StreamReader(path))
+            {   //getting json file to show on screen
+                dynamic json = r.ReadToEnd();
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var items in array)
+                {
+                    Console.WriteLine(items);
+                }
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,29 @@
+1. Name 
+#Lab 09 LINQ
+
+2. One-Liner
+Converting JSON File, showing them on the console and filter via LINQ and Lambda expressions.
+
+3. Usage
+
+a. First install appropriate programs. Refer to Step 5 in this README.
+b. Upon start up, the deck will instantiate with chosen cards.
+c. The user will have the choice of being able to add a card, remove or shuffle.
+d. Once any of the choices have been chosen and completed, the program will close.
+
+Visual
+![Image](asset/Lab09.JPG)
+
+4. API
+No API's were harmed in the making of this.
+
+5. Installation
+Install Visual Studio 2017 and .NET Core SDK. NewtonSoft extension.
+
+6. License
+This program's License is MIT.
+
+7. Acknowledgements - I worked with 
+- http://github.com/jcqnly 
+- Jesse Atay
+- Judah Hunger
